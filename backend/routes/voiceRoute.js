65c19bd40e0dc6550d8b188348e0a09211e66196
tshepoandi <lovekilledtshepo@gmail.com>
2024-09-23 12:24:06 +0200

    added routes

---
+++ backend/routes/voiceRoute.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import ElevenLabsController from '../controllers/VoicesController'
+
+const router = express.Router()
+const elevenLabsController = new ElevenLabsController()
+
+router.get('/voices', elevenLabsController.getVoices)
+router.post('/text-to-speech', elevenLabsController.createTextToSpeech)
+
+export default router
