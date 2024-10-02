e16bb2ce1f8591e43d0b98e2e87d32f605c85637
tshepoandi <lovekilledtshepo@gmail.com>
2024-10-02 08:49:06 +0200

    download section refactoring and preparation

--- frontend/Pages/DownloadSection.jsx
+++ frontend/Pages/DownloadSection.jsx
@@ -3,9 +3,6 @@ import { useSelector } from 'react-redux'
 const DownloadSection = () => {
   const selectedSong = useSelector((state) => state.song.selectedSong)
 
-  // Here you can use the selectedSong data to interact with your Gemini API
-  // and fetch lyrics or perform other operations
-
   return (
     <div>
       <h1>Download Page</h1>
@@ -13,7 +10,6 @@ const DownloadSection = () => {
         <div>
           <h2>{selectedSong.title}</h2>
           <p>Artist: {selectedSong.artist}</p>
-          {/* Add more details or functionality here */}
         </div>
       )}
     </div>
