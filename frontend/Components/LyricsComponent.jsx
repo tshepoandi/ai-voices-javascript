84db0269ac8bc437c45fa4ebc456ce923b3b54a8
tshepoandi <lovekilledtshepo@gmail.com>
2024-10-10 09:31:14 +0200

    refactoring

--- frontend/Components/LyricsComponent.jsx
+++ frontend/Components/LyricsComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
 import styled from 'styled-components'
