b10b0fef6435b3d52dca9b3b7f1e43c45cf18d33
tshepoandi <lovekilledtshepo@gmail.com>
2024-10-10 14:13:42 +0200

    everything works

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,14 @@ import cors from 'cors'
 import voiceRoutes from '../routes/voiceRoute.js'
 import geniusRoutes from '../routes/geniusRoute.js'
 import scriptRoute from '../routes/geminiRoute.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import fs from 'fs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const app = express()
 const port = 3000
 
@@ -12,10 +20,12 @@ app.use(express.json())
 app.use('/voice', voiceRoutes)
 app.use('/genius', geniusRoutes)
 app.use('/gemini', scriptRoute)
-
 app.get('/download', (req, res) => {
   const filePath = req.query.file
-  const fullPath = path.join(__dirname, filePath)
+  const backendDir = path.join(__dirname, '../') // Move up one level to backend folder
+  const fullPath = path.join(backendDir, filePath)
+
+  console.log(`Checking for file: ${fullPath}`)
 
   fs.access(fullPath, fs.constants.F_OK, (err) => {
     if (err) {
@@ -31,7 +41,6 @@ app.get('/download', (req, res) => {
     })
   })
 })
-
 app.listen(port, () => {
   console.log(`check this out at http://localhost:${port}`)
 })
